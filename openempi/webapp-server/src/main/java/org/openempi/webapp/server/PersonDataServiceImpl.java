843decf15962d39f180a33129314fc340982cf72
Csaba Toth <csaba.toth@vanderbilt.edu>
2013-05-30 07:14:29 -0700

    Authenticating with KeyServer for BF Reencoding/RBF test

--- openempi/webapp-server/src/main/java/org/openempi/webapp/server/PersonDataServiceImpl.java
+++ openempi/webapp-server/src/main/java/org/openempi/webapp/server/PersonDataServiceImpl.java
@@ -329,6 +329,8 @@ public class PersonDataServiceImpl extends RemoteServiceServlet implements Perso
 //			recordLinkageProtocol.testPMLinkRecords(dataset.getDatasetId(), datasetFound.getDatasetId(), Constants.BLOCKING_BYPASS_SERVICE_NAME, Constants.CBF_SCORING_SERVICE_NAME);
 
 			// BF Recode test
+			KeyServerService ks = Context.getKeyServerService();
+			ks.authenticate(Constants.DEFAULT_ADMIN_USERNAME, Constants.DEFAULT_ADMIN_PASSWORD);
 			RecordLinkageProtocolSelector recordLinkageProtocolSelector = Context.getRecordLinkageProtocolSelector();
 			RecordLinkageProtocolType recordLinkageProtocolType = recordLinkageProtocolSelector.getRecordLinkageProtocolType(Constants.THREE_THIRD_PARTY_CBF_W_RND_BLOCKING_PROTOCOL_NAME);
 			RecordLinkageProtocol recordLinkageProtocol = recordLinkageProtocolType.getRecordLinkageProtocol();
