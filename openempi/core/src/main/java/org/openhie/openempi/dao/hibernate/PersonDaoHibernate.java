76630127dc8a4f4b21018f93c9dc06cbefa3d1e3
Csaba Toth <csaba.toth@vanderbilt.edu>
2013-06-06 18:45:30 -0700

    Removing unnecessary byte->string conversion map after the last commit. Commention out some string checks in the name of speed. The check is still there for table creation. Maybe revisit later if I want to readd some.

--- openempi/core/src/main/java/org/openhie/openempi/dao/hibernate/PersonDaoHibernate.java
+++ openempi/core/src/main/java/org/openhie/openempi/dao/hibernate/PersonDaoHibernate.java
@@ -20,8 +20,6 @@ package org.openhie.openempi.dao.hibernate;
 import java.math.BigInteger;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -41,282 +39,6 @@ import org.springframework.orm.hibernate3.HibernateCallback;
 
 public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonDao
 {
-	private static final Map<Byte, String> BYTE_TO_OCTA_STRING_MAP =
-	      Collections.unmodifiableMap(new HashMap<Byte, String>() {{
-	        put((byte)0x00, "\\\\000");
-	        put((byte)0x01, "\\\\001");
-	        put((byte)0x02, "\\\\002");
-	        put((byte)0x03, "\\\\003");
-	        put((byte)0x04, "\\\\004");
-	        put((byte)0x05, "\\\\005");
-	        put((byte)0x06, "\\\\006");
-	        put((byte)0x07, "\\\\007");
-	        put((byte)0x08, "\\\\010");
-	        put((byte)0x09, "\\\\011");
-	        put((byte)0x0A, "\\\\012");
-	        put((byte)0x0B, "\\\\013");
-	        put((byte)0x0C, "\\\\014");
-	        put((byte)0x0D, "\\\\015");
-	        put((byte)0x0E, "\\\\016");
-	        put((byte)0x0F, "\\\\017");
-
-	        put((byte)0x10, "\\\\020");
-	        put((byte)0x11, "\\\\021");
-	        put((byte)0x12, "\\\\022");
-	        put((byte)0x13, "\\\\023");
-	        put((byte)0x14, "\\\\024");
-	        put((byte)0x15, "\\\\025");
-	        put((byte)0x16, "\\\\026");
-	        put((byte)0x17, "\\\\027");
-	        put((byte)0x18, "\\\\030");
-	        put((byte)0x19, "\\\\031");
-	        put((byte)0x1A, "\\\\032");
-	        put((byte)0x1B, "\\\\033");
-	        put((byte)0x1C, "\\\\034");
-	        put((byte)0x1D, "\\\\035");
-	        put((byte)0x1E, "\\\\036");
-	        put((byte)0x1F, "\\\\037");
-
-	        put((byte)0x20, "\\\\040");
-	        put((byte)0x21, "\\\\041");
-	        put((byte)0x22, "\\\\042");
-	        put((byte)0x23, "\\\\043");
-	        put((byte)0x24, "\\\\044");
-	        put((byte)0x25, "\\\\045");
-	        put((byte)0x26, "\\\\046");
-	        put((byte)0x27, "\\\\047");
-	        put((byte)0x28, "\\\\050");
-	        put((byte)0x29, "\\\\051");
-	        put((byte)0x2A, "\\\\052");
-	        put((byte)0x2B, "\\\\053");
-	        put((byte)0x2C, "\\\\054");
-	        put((byte)0x2D, "\\\\055");
-	        put((byte)0x2E, "\\\\056");
-	        put((byte)0x2F, "\\\\057");
-
-	        put((byte)0x30, "\\\\060");
-	        put((byte)0x31, "\\\\061");
-	        put((byte)0x32, "\\\\062");
-	        put((byte)0x33, "\\\\063");
-	        put((byte)0x34, "\\\\064");
-	        put((byte)0x35, "\\\\065");
-	        put((byte)0x36, "\\\\066");
-	        put((byte)0x37, "\\\\067");
-	        put((byte)0x38, "\\\\070");
-	        put((byte)0x39, "\\\\071");
-	        put((byte)0x3A, "\\\\072");
-	        put((byte)0x3B, "\\\\073");
-	        put((byte)0x3C, "\\\\074");
-	        put((byte)0x3D, "\\\\075");
-	        put((byte)0x3E, "\\\\076");
-	        put((byte)0x3F, "\\\\077");
-
-	        put((byte)0x40, "\\\\100");
-	        put((byte)0x41, "\\\\101");
-	        put((byte)0x42, "\\\\102");
-	        put((byte)0x43, "\\\\103");
-	        put((byte)0x44, "\\\\104");
-	        put((byte)0x45, "\\\\105");
-	        put((byte)0x46, "\\\\106");
-	        put((byte)0x47, "\\\\107");
-	        put((byte)0x48, "\\\\110");
-	        put((byte)0x49, "\\\\111");
-	        put((byte)0x4A, "\\\\112");
-	        put((byte)0x4B, "\\\\113");
-	        put((byte)0x4C, "\\\\114");
-	        put((byte)0x4D, "\\\\115");
-	        put((byte)0x4E, "\\\\116");
-	        put((byte)0x4F, "\\\\117");
-
-	        put((byte)0x50, "\\\\120");
-	        put((byte)0x51, "\\\\121");
-	        put((byte)0x52, "\\\\122");
-	        put((byte)0x53, "\\\\123");
-	        put((byte)0x54, "\\\\124");
-	        put((byte)0x55, "\\\\125");
-	        put((byte)0x56, "\\\\126");
-	        put((byte)0x57, "\\\\127");
-	        put((byte)0x58, "\\\\130");
-	        put((byte)0x59, "\\\\131");
-	        put((byte)0x5A, "\\\\132");
-	        put((byte)0x5B, "\\\\133");
-	        put((byte)0x5C, "\\\\134");
-	        put((byte)0x5D, "\\\\135");
-	        put((byte)0x5E, "\\\\136");
-	        put((byte)0x5F, "\\\\137");
-
-	        put((byte)0x60, "\\\\140");
-	        put((byte)0x61, "\\\\141");
-	        put((byte)0x62, "\\\\142");
-	        put((byte)0x63, "\\\\143");
-	        put((byte)0x64, "\\\\144");
-	        put((byte)0x65, "\\\\145");
-	        put((byte)0x66, "\\\\146");
-	        put((byte)0x67, "\\\\147");
-	        put((byte)0x68, "\\\\150");
-	        put((byte)0x69, "\\\\151");
-	        put((byte)0x6A, "\\\\152");
-	        put((byte)0x6B, "\\\\153");
-	        put((byte)0x6C, "\\\\154");
-	        put((byte)0x6D, "\\\\155");
-	        put((byte)0x6E, "\\\\156");
-	        put((byte)0x6F, "\\\\157");
-
-	        put((byte)0x70, "\\\\160");
-	        put((byte)0x71, "\\\\161");
-	        put((byte)0x72, "\\\\162");
-	        put((byte)0x73, "\\\\163");
-	        put((byte)0x74, "\\\\164");
-	        put((byte)0x75, "\\\\165");
-	        put((byte)0x76, "\\\\166");
-	        put((byte)0x77, "\\\\167");
-	        put((byte)0x78, "\\\\170");
-	        put((byte)0x79, "\\\\171");
-	        put((byte)0x7A, "\\\\172");
-	        put((byte)0x7B, "\\\\173");
-	        put((byte)0x7C, "\\\\174");
-	        put((byte)0x7D, "\\\\175");
-	        put((byte)0x7E, "\\\\176");
-	        put((byte)0x7F, "\\\\177");
-
-	        put((byte)0x80, "\\\\200");
-	        put((byte)0x81, "\\\\201");
-	        put((byte)0x82, "\\\\202");
-	        put((byte)0x83, "\\\\203");
-	        put((byte)0x84, "\\\\204");
-	        put((byte)0x85, "\\\\205");
-	        put((byte)0x86, "\\\\206");
-	        put((byte)0x87, "\\\\207");
-	        put((byte)0x88, "\\\\210");
-	        put((byte)0x89, "\\\\211");
-	        put((byte)0x8A, "\\\\212");
-	        put((byte)0x8B, "\\\\213");
-	        put((byte)0x8C, "\\\\214");
-	        put((byte)0x8D, "\\\\215");
-	        put((byte)0x8E, "\\\\216");
-	        put((byte)0x8F, "\\\\217");
-
-	        put((byte)0x90, "\\\\220");
-	        put((byte)0x91, "\\\\221");
-	        put((byte)0x92, "\\\\222");
-	        put((byte)0x93, "\\\\223");
-	        put((byte)0x94, "\\\\224");
-	        put((byte)0x95, "\\\\225");
-	        put((byte)0x96, "\\\\226");
-	        put((byte)0x97, "\\\\227");
-	        put((byte)0x98, "\\\\230");
-	        put((byte)0x99, "\\\\231");
-	        put((byte)0x9A, "\\\\232");
-	        put((byte)0x9B, "\\\\233");
-	        put((byte)0x9C, "\\\\234");
-	        put((byte)0x9D, "\\\\235");
-	        put((byte)0x9E, "\\\\236");
-	        put((byte)0x9F, "\\\\237");
-
-	        put((byte)0xA0, "\\\\240");
-	        put((byte)0xA1, "\\\\241");
-	        put((byte)0xA2, "\\\\242");
-	        put((byte)0xA3, "\\\\243");
-	        put((byte)0xA4, "\\\\244");
-	        put((byte)0xA5, "\\\\245");
-	        put((byte)0xA6, "\\\\246");
-	        put((byte)0xA7, "\\\\247");
-	        put((byte)0xA8, "\\\\250");
-	        put((byte)0xA9, "\\\\251");
-	        put((byte)0xAA, "\\\\252");
-	        put((byte)0xAB, "\\\\253");
-	        put((byte)0xAC, "\\\\254");
-	        put((byte)0xAD, "\\\\255");
-	        put((byte)0xAE, "\\\\256");
-	        put((byte)0xAF, "\\\\257");
-
-	        put((byte)0xB0, "\\\\260");
-	        put((byte)0xB1, "\\\\261");
-	        put((byte)0xB2, "\\\\262");
-	        put((byte)0xB3, "\\\\263");
-	        put((byte)0xB4, "\\\\264");
-	        put((byte)0xB5, "\\\\265");
-	        put((byte)0xB6, "\\\\266");
-	        put((byte)0xB7, "\\\\267");
-	        put((byte)0xB8, "\\\\270");
-	        put((byte)0xB9, "\\\\271");
-	        put((byte)0xBA, "\\\\272");
-	        put((byte)0xBB, "\\\\273");
-	        put((byte)0xBC, "\\\\274");
-	        put((byte)0xBD, "\\\\275");
-	        put((byte)0xBE, "\\\\276");
-	        put((byte)0xBF, "\\\\277");
-
-	        put((byte)0xC0, "\\\\300");
-	        put((byte)0xC1, "\\\\301");
-	        put((byte)0xC2, "\\\\302");
-	        put((byte)0xC3, "\\\\303");
-	        put((byte)0xC4, "\\\\304");
-	        put((byte)0xC5, "\\\\305");
-	        put((byte)0xC6, "\\\\306");
-	        put((byte)0xC7, "\\\\307");
-	        put((byte)0xC8, "\\\\310");
-	        put((byte)0xC9, "\\\\311");
-	        put((byte)0xCA, "\\\\312");
-	        put((byte)0xCB, "\\\\313");
-	        put((byte)0xCC, "\\\\314");
-	        put((byte)0xCD, "\\\\315");
-	        put((byte)0xCE, "\\\\316");
-	        put((byte)0xCF, "\\\\317");
-
-	        put((byte)0xD0, "\\\\320");
-	        put((byte)0xD1, "\\\\321");
-	        put((byte)0xD2, "\\\\322");
-	        put((byte)0xD3, "\\\\323");
-	        put((byte)0xD4, "\\\\324");
-	        put((byte)0xD5, "\\\\325");
-	        put((byte)0xD6, "\\\\326");
-	        put((byte)0xD7, "\\\\327");
-	        put((byte)0xD8, "\\\\330");
-	        put((byte)0xD9, "\\\\331");
-	        put((byte)0xDA, "\\\\332");
-	        put((byte)0xDB, "\\\\333");
-	        put((byte)0xDC, "\\\\334");
-	        put((byte)0xDD, "\\\\335");
-	        put((byte)0xDE, "\\\\336");
-	        put((byte)0xDF, "\\\\337");
-
-	        put((byte)0xE0, "\\\\340");
-	        put((byte)0xE1, "\\\\341");
-	        put((byte)0xE2, "\\\\342");
-	        put((byte)0xE3, "\\\\343");
-	        put((byte)0xE4, "\\\\344");
-	        put((byte)0xE5, "\\\\345");
-	        put((byte)0xE6, "\\\\346");
-	        put((byte)0xE7, "\\\\347");
-	        put((byte)0xE8, "\\\\350");
-	        put((byte)0xE9, "\\\\351");
-	        put((byte)0xEA, "\\\\352");
-	        put((byte)0xEB, "\\\\353");
-	        put((byte)0xEC, "\\\\354");
-	        put((byte)0xED, "\\\\355");
-	        put((byte)0xEE, "\\\\356");
-	        put((byte)0xEF, "\\\\357");
-
-	        put((byte)0xF0, "\\\\360");
-	        put((byte)0xF1, "\\\\361");
-	        put((byte)0xF2, "\\\\362");
-	        put((byte)0xF3, "\\\\363");
-	        put((byte)0xF4, "\\\\364");
-	        put((byte)0xF5, "\\\\365");
-	        put((byte)0xF6, "\\\\366");
-	        put((byte)0xF7, "\\\\367");
-	        put((byte)0xF8, "\\\\370");
-	        put((byte)0xF9, "\\\\371");
-	        put((byte)0xFA, "\\\\372");
-	        put((byte)0xFB, "\\\\373");
-	        put((byte)0xFC, "\\\\374");
-	        put((byte)0xFD, "\\\\375");
-	        put((byte)0xFE, "\\\\376");
-	        put((byte)0xFF, "\\\\377");
-
-	      }});
-
 	public void createTable(final String tableName, final List<ColumnInformation> columnInformation,
 			final boolean withIndexesAndConstraints) {
 		log.trace("Creating table " + tableName);
@@ -665,7 +387,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 				Iterator<Entry<String, Object>> it = attributes.entrySet().iterator();
 				while (it.hasNext()) {
 					Map.Entry<String, Object> pairs = (Map.Entry<String, Object>)it.next();
-					ValidationUtil.sanityCheckFieldName(pairs.getKey());
+					//ValidationUtil.sanityCheckFieldName(pairs.getKey());
 					String columnName = pairs.getKey().toLowerCase();
 					if (pairs.getValue() == null)
 						sqlQueryString.append(COLUMN_NAME_PREFIX + columnName + "=null");
@@ -701,7 +423,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 	
 	private void generateNotNullSqlCriteriaFromFields(StringBuilder sqlSelect, List<String> fields) {
 		for (String field : fields) {
-			ValidationUtil.sanityCheckFieldName(field);
+			//ValidationUtil.sanityCheckFieldName(field);
 			sqlSelect.append("AND ");
 			sqlSelect.append(COLUMN_NAME_PREFIX + field);
 			sqlSelect.append(" IS NOT NULL ");
@@ -725,7 +447,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 	private void appendFieldsToSelectFromFieldList(StringBuilder sqlSelect, List<String> fields) {
 		sqlSelect.append(PERSON_ID_COLUMN_NAME);
 		for (String field : fields) {
-			ValidationUtil.sanityCheckFieldName(field);
+			//ValidationUtil.sanityCheckFieldName(field);
 			sqlSelect.append(", ");
 			sqlSelect.append(COLUMN_NAME_PREFIX + field);
 		}
@@ -785,7 +507,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 				StringBuilder sqlSelect = new StringBuilder("SELECT DISTINCT ");
 				StringBuilder sqlSelectPart2 = new StringBuilder(" ORDER BY ");
 				for (String field : fields) {
-					ValidationUtil.sanityCheckFieldName(field);
+					//ValidationUtil.sanityCheckFieldName(field);
 					sqlSelect.append(COLUMN_NAME_PREFIX + field.toLowerCase() + ", ");
 					sqlSelectPart2.append(COLUMN_NAME_PREFIX + field.toLowerCase() + " ASC, ");
 				}
@@ -823,7 +545,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 		if (field == null || field.length() == 0) {
 			return 0.0;
 		}
-		ValidationUtil.sanityCheckFieldName(field);
+		//ValidationUtil.sanityCheckFieldName(field);
 		return (Double) getHibernateTemplate().execute(new HibernateCallback() {
 			public Object doInHibernate(Session session) throws HibernateException, SQLException {
 				String fieldLowerCase = field.toLowerCase();
@@ -849,7 +571,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 		if (field == null || field.length() == 0) {
 			return 0.0;
 		}
-		ValidationUtil.sanityCheckFieldName(field);
+		//ValidationUtil.sanityCheckFieldName(field);
 		return  (Double) getHibernateTemplate().execute(new HibernateCallback() {
 			public Object doInHibernate(Session session) throws HibernateException, SQLException {
 				return getFieldMissingRatioInHibernate(session, tableName, field, true);
@@ -870,7 +592,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 		if (field == null || field.length() == 0) {
 			return 0;
 		}
-		ValidationUtil.sanityCheckFieldName(field);
+		//ValidationUtil.sanityCheckFieldName(field);
 		return (Integer) getHibernateTemplate().execute(new HibernateCallback() {
 			public Object doInHibernate(Session session) throws HibernateException, SQLException {
 				String fieldLowerCase = field.toLowerCase();
@@ -887,7 +609,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 	}
 
 	private double getFieldMissingRatioInHibernate(Session session, String tableName, String field, boolean missingOrPresent) {
-		ValidationUtil.sanityCheckFieldName(field);
+		//ValidationUtil.sanityCheckFieldName(field);
 		int numOfNotNull = getFieldPresentNumberInHibernate(session, tableName, field);
 		long totalNum = getTotalNumberOfRecordsInHibernate(session, tableName);
 		if (totalNum == 0L) {
@@ -906,7 +628,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 	}
 
 	private int getFieldPresentNumberInHibernate(Session session, String tableName, String field) {
-		ValidationUtil.sanityCheckFieldName(field);
+		//ValidationUtil.sanityCheckFieldName(field);
 		String fieldLowerCase = field.toLowerCase();
 		StringBuilder sqlSelect = new StringBuilder("SELECT COUNT(");
 		sqlSelect.append(COLUMN_NAME_PREFIX + fieldLowerCase);
@@ -931,7 +653,7 @@ public class PersonDaoHibernate extends UniversalDaoHibernate implements PersonD
 	}
 
 	public String getTableFullName(String tableName) {
-		ValidationUtil.sanityCheckFieldName(tableName);
+		//ValidationUtil.sanityCheckFieldName(tableName);
 		return DATASET_TABLE_NAME_PREFIX + tableName;
 	}
 
